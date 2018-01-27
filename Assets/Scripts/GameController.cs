3948a2b1580ee91310bc4b19232f1e423ea683f3
Vincent <swa.vincent@gmail.com>
2018-01-27 13:48:46 -0500

    Add Game Controller

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,10 +4,40 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    [System.Serializable]
+    public struct LevelStruct
+    {
+        public AudioClip clip;
+        public float[] pitchs;
+    }
+
+    public static GameController Instance { get; private set; }
+
+    [SerializeField]
+    List<LevelStruct> levels;
+
+    [SerializeField]
+    int levelToLoadAtStart = 0;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.LogWarning("Game Controller already exist");
+            enabled = false;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+#if !UNITY_EDITOR
+        levelToLoadAtStart = 0;
+#endif
+    }
 	
 	// Update is called once per frame
 	void Update () {
